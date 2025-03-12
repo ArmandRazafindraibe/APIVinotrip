8a150c47cc8fa547a6662a7be2eb1cf4a4f696d7
lebretj <jordan.lebret@etu.univ-smb.fr>
2025-03-12 11:45:31 +0100

    Code first : Jordan

---
+++ APIVinbotrip/Models/Entity Framework/Activite.cs
@@ -0,0 +1,26 @@
+﻿using APIVinbotrip.Models.Entity_Framework;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIVinotrip.Models.Entity_Framework
+{
+    [Table("ACTIVITE")]
+    public partial class Activite
+    {
+
+        [Key]
+        [Column("idActivite")]
+        public int IdActivite { get; set; }
+
+        [Column("libelleActivite")]
+        [StringLength(100)]
+        public string? LibelleActivite { get; set; }
+
+        [Column("prixActivite", TypeName = "NUMERIC(8,2)")]
+        public decimal? PrixActivite { get; set; }
+
+        [InverseProperty(nameof(Comporte.ActiviteComporte))]
+        public virtual ICollection<Comporte> ListeActivites { get; set; } = new List<Comporte>();
+
+    }
+}
