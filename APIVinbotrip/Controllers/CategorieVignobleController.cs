364e1b4c1ad8e39605c82df30b89a841da65f3a8
Luzagar <luzagar.abonner@gmail.com>
2025-03-20 20:41:18 +0100

    Ajout controller CategeorieVignoble

---
+++ APIVinbotrip/Controllers/CategorieVignobleController.cs
@@ -0,0 +1,125 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIVinotrip.Models.EntityFramework;
+using APIVinotrip.Models.DataManager;
+using APIVinotrip.Models.Repository;
+
+namespace APIVinotrip.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategorieVignobleController : ControllerBase
+    {
+        private readonly IDataRepository<CategorieVignoble> dataRepository;
+
+        public CategorieVignobleController(IDataRepository<CategorieVignoble> dataRepos)
+        {
+            dataRepository = dataRepos;
+        }
+
+        // GET: api/Aviss
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategorieVignoble>>> GetCategorieVignobles()
+        {
+            return await dataRepository.GetAll();
+        }
+
+        // GET: api/Aviss/5
+        [HttpGet]
+        [Route("[action]/{id}")]
+        [ActionName("GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<CategorieVignoble>> GetCategorieVignobleById(int id)
+        {
+            var avis = await dataRepository.GetById(id);
+
+            if (avis == null)
+            {
+                return NotFound();
+            }
+
+            return avis;
+        }
+
+        // GET: api/Aviss/5
+        [HttpGet]
+        [Route("[action]/{title}")]
+        [ActionName("GetAvisByTitle")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<CategorieVignoble>> GetCategorieVignobleByTitle(string title)
+        {
+            var avis = await dataRepository.GetByString(title);
+
+            if (avis == null)
+            {
+                return NotFound();
+            }
+
+            return avis;
+        }
+
+        // PUT: api/Aviss/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> PutCategorieVignoble(int id, CategorieVignoble categorieVignoble)
+        {
+            if (id != categorieVignoble.IdCategorieVignoble)
+            {
+                return BadRequest();
+            }
+
+            var userToUpdate = await dataRepository.GetById(id);
+            if (userToUpdate == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                await dataRepository.Update(userToUpdate.Value, categorieVignoble);
+                return NoContent();
+            }
+        }
+
+        // POST: api/Aviss
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Avis>> PostCategorieVignoble(CategorieVignoble categorieVignoble)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await dataRepository.Add(categorieVignoble);
+
+            return CreatedAtAction("GetById", new { id = categorieVignoble.IdCategorieVignoble }, categorieVignoble); // GetById : nom de l’action
+        }
+
+        // DELETE: api/Aviss/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteCategorieVignoble(int id)
+        {
+            var avis = await dataRepository.GetById(id);
+            if (avis == null)
+            {
+                return NotFound();
+            }
+            await dataRepository.Delete(avis.Value);
+            return NoContent();
+        }
+
+        //private bool AvisExists(int id)
+        //{
+        //    return _context.Aviss.Any(e => e.Idavis == id);
+        //}
+    }
+}
---
+++ APIVinbotrip/Models/DataManager/CategorieVignobleManager.cs
@@ -0,0 +1,46 @@
+using APIVinotrip.Models.EntityFramework;
+using APIVinotrip.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIVinotrip.Models.DataManager
+{
+    public class CategorieVignobleManager : IDataRepository<CategorieVignoble>
+    {
+        readonly DBVinotripContext? vinotripDBContext;
+        public CategorieVignobleManager() { }
+        public CategorieVignobleManager(DBVinotripContext context)
+        {
+            vinotripDBContext = context;
+        }
+        public async Task<ActionResult<IEnumerable<CategorieVignoble>>> GetAll()
+        {
+            return vinotripDBContext.Categorievignobles.ToList();
+        }
+        public async Task<ActionResult<CategorieVignoble>> GetById(int id)
+        {
+            return vinotripDBContext.Categorievignobles.FirstOrDefault(u => u.IdCategorieVignoble == id);
+        }
+        public async Task<ActionResult<CategorieVignoble>> GetByString(string mail)
+        {
+            return vinotripDBContext.Categorievignobles.FirstOrDefault(u => u.LibelleCategorieVignoble.ToUpper() == mail.ToUpper());
+        }
+        public async Task Add(CategorieVignoble entity)
+        {
+            vinotripDBContext.Categorievignobles.Add(entity);
+            vinotripDBContext.SaveChanges();
+        }
+        public async Task Update(CategorieVignoble categorieVignoble, CategorieVignoble entity)
+        {
+            vinotripDBContext.Entry(categorieVignoble).State = EntityState.Modified;
+            categorieVignoble.IdCategorieVignoble = entity.IdCategorieVignoble;
+            categorieVignoble.LibelleCategorieVignoble = entity.LibelleCategorieVignoble;
+            vinotripDBContext.SaveChanges();
+        }
+        public async Task Delete(CategorieVignoble categorieVignoble)
+        {
+            vinotripDBContext.Categorievignobles.Remove(categorieVignoble);
+            vinotripDBContext.SaveChanges();
+        }
+    }
+}
