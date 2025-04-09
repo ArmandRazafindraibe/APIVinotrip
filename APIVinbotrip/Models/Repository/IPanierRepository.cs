fb843bbbe57d5e888dc8f7bd91fb45bdb6e06fc5
Luzagar <luzagar.abonner@gmail.com>
2025-04-09 20:36:16 +0200

    giga controller

--- APIVinbotrip/Models/Repository/IPanierRepository.cs
+++ APIVinbotrip/Models/Repository/IPanierRepository.cs
@@ -9,5 +9,6 @@ namespace APIVinotrip.Models.Repository
         Task UpdateDetailPanier(DescriptionPanier desc, DescriptionPanier entity);
         Task<ActionResult<IEnumerable<DescriptionPanier>>> GetDescriptionsPanierById(int id);
         Task<ActionResult<DescriptionPanier>> GetOneDescriptionPanierById(int id);
+        Task<ActionResult<IEnumerable<DescriptionPanier>>> GetAllDescriptionPanierDetail(int idPanier);
     }
 }
